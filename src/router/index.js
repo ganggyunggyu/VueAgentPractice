44c07bfcc5bf396034cdf192907aa1fe632050d3
강경규 <qwzx16@naver.com>
2024-06-19 19:03:11 +0900

    routing add

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,14 @@ import Capture from '@views/Capture.vue';
 import Landing from '@views/Landing.vue';
 import Intro from '@views/Intro.vue';
 import Loading from '@views/Loading.vue';
+import Stage from '@views/Stage.vue';
 
 export const routes = [
   { path: '/', component: Landing, name: 'Landing' },
   { path: '/capture', component: Capture, name: 'Capture' },
   { path: '/intro', component: Intro, name: 'Intro' },
   { path: '/loading', component: Loading, name: 'Loading' },
+  { path: '/stage', component: Stage, name: 'Stage' },
 ];
 
 const router = createRouter({

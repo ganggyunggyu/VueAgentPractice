4ad3ff517cb59d0bf4c0c023f4a8fff43fe24e0e
강경규 <qwzx16@naver.com>
2024-06-19 17:10:24 +0900

    feat: routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,14 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
+import Capture from '@views/Capture.vue';
+import Landing from '@views/Landing.vue';
+import Intro from '@views/Intro.vue';
+import Loading from '@views/Loading.vue';
 
 export const routes = [
-  // { path: '/', component: Landing, name: 'Landing' },
-  // { path: '/capture', component: Capture, name: 'Capture' },
-  // { path: '/intro', component: Intro, name: 'Intro' },
-  // { path: '/commonone3d', component: CommoneOne3D, name: 'CommoneOne3D' },
-  // { path: '/storagebox', component: StorageBox, name: 'StorageBox' },
-  // { path: '/capturereview', component: CaptureReview, name: 'CaptureReview' },
-  // { path: '/storagebox2', component: StorageBox2, name: 'StorageBox2' },
-  // { path: '/stage', component: Stage, name: 'Stage' },
-  // { path: '/commonfour', component: CommonFour, name: 'CommonFour' },
-  // { path: '/event1', component: Event1, name: 'Event1' },
-  // { path: '/event2', component: Event2, name: 'Event2' },
-  // { path: '/event3', component: Event3, name: 'Event3' },
-  // { path: '/event4', component: Event4, name: 'Event4' },
-  // { path: '/event5', component: Event5, name: 'Event5' },
-  // { path: '/event6', component: Event6, name: 'Event6' },
-  // { path: '/event7', component: Event7, name: 'Event7' },
-  // { path: '/event8', component: Event8, name: 'Event8' },
-  // { path: '/event9', component: Event9, name: 'Event9' },
-  // { path: '/event10', component: Event10, name: 'Event10' },
-  // { path: '/eventout', component: EventOut, name: 'EventOut' },
-  // { path: '/culture3', component: Culture2nd3D, name: 'Culture2nd3D' },
-  // { path: '/capturepreview', component: CapturePreview, name: 'CaptruePreview' },
-  // { path: '/culture3d', component: Culture3D, name: 'Culture3D' },
-  // { path: '/loading', component: Loading, name: 'Loading' },
-  // { path: '/ios', component: Ios, name: 'Ios' },
-  // { path: '/mindimage', component: MindImage, name: 'MindImage' },
-  // { path: '/busan2', component: busan2, name: 'busan2' },
-  // { path: '/busan3', component: busan3, name: 'busan3' },
-  // { path: '/coex', component: coex, name: 'coex' },
-  // { path: '/plane', component: plane, name: 'plane' },
+  { path: '/', component: Landing, name: 'Landing' },
+  { path: '/capture', component: Capture, name: 'Capture' },
+  { path: '/intro', component: Intro, name: 'Intro' },
+  { path: '/loading', component: Loading, name: 'Loading' },
 ];
 
 const router = createRouter({

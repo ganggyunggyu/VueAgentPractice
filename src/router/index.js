292dd875c4551ea4eef1813888f87b31174abce9
ganggyunggyu <qwzx16@naver.com>
2024-06-20 18:46:21 +0900

    feat: routing 추가

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import Landing from '@views/Landing.vue';
 import Intro from '@views/Intro.vue';
 import Loading from '@views/Loading.vue';
 import Stage from '@views/Stage.vue';
+import NormalStorage from '@views/NormalStorage.vue';
+import PrimiumStorage from '@views/PrimiumStorage.vue';
 
 export const routes = [
   { path: '/', component: Landing, name: 'Landing' },
@@ -11,6 +13,8 @@ export const routes = [
   { path: '/intro', component: Intro, name: 'Intro' },
   { path: '/loading', component: Loading, name: 'Loading' },
   { path: '/stage', component: Stage, name: 'Stage' },
+  { path: '/normal-storage', component: NormalStorage, name: 'NomalStorage' },
+  { path: '/primium-storage', component: PrimiumStorage, name: 'PrimiumStorage' },
 ];
 
 const router = createRouter({

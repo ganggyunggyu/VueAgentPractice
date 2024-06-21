5e8613b2b9aa50e3696a2340c67df194bd0a79c2
강경규 <qwzx16@naver.com>
2024-06-21 09:37:29 +0900

    feat: 버튼 함수 생성 후 사용

--- src/features/stage/BottomNavigation.vue
+++ src/features/stage/BottomNavigation.vue
@@ -1,48 +1,58 @@
 <script setup>
 import BottomNavigationItem from '@/entities/stage/ui/BottomNavigationItem.vue';
-import Home from '@/shared/ui/Home.vue'
+import Home from '@/shared/ui/Home.vue';
 </script>
 
 <template>
   <nav class="bottom-nav">
-    <BottomNavigationItem @click="this.$router.replace('normal-storage')" icon="../../../../static/resource/reward/reward_active.png" lable="일반 보상"/>
+    <BottomNavigationItem
+      @click="redirectNomalStorage"
+      icon="../../../../static/resource/reward/reward_active.png"
+      lable="일반 보상"
+    />
     <button @click="this.$router.replace('stage')">
       <Home class="home-icon" />
     </button>
-    <BottomNavigationItem @click="this.$router.replace('primium-storage')" icon="../../../../static/resource/reward/premium_active.png" lable="프리미엄 보상"/>
+    <BottomNavigationItem
+      @click="redirectPrimiumStorage"
+      icon="../../../../static/resource/reward/premium_active.png"
+      lable="프리미엄 보상"
+    />
   </nav>
 </template>
 
 <script>
+import { playAudio } from '@/shared/lib/audio';
 export default {
   name: 'BottomNavigation',
 
-  setup(){
-    const redirectNomalStorage = () => {
-      this.$router.replace('normal-storage')
-    }
-    const redirectPrimiumStorage = () => {
-      this.$router.replace('primium-storage')
-    }
-  }
-
+  methods: {
+    redirectNomalStorage() {
+      this.$router.replace('normal-storage');
+      playAudio();
+    },
+    redirectPrimiumStorage() {
+      this.$router.replace('primium-storage');
+      playAudio();
+    },
+  },
 };
 </script>
 
 <style scoped>
-nav{
+nav {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   width: 100%;
-  align-items: center; 
-  justify-items: center; 
+  align-items: center;
+  justify-items: center;
   background-color: var(--color-red);
   position: fixed;
   bottom: 0;
   left: 0;
   padding: 10px;
 }
-button{
+button {
   width: 80px;
   height: 80px;
   border-radius: 50%;
@@ -51,9 +61,9 @@ button{
   border: 3px solid var(--color-red);
   transform: translateY(-25px);
   box-shadow: 0 5px 5px rgba(0, 0, 0, 0.2);
-
 }
-.home-icon{
+
+.home-icon {
   width: 90%;
   height: 90%;
 }
---
+++ src/shared/lib/audio.js
@@ -0,0 +1,10 @@
+export const playAudio = () => {
+  import('../../../static/resource/sounds/generaltap.wav')
+    .then((module) => {
+      const audio = new Audio(module.default);
+      audio.play();
+    })
+    .catch((error) => {
+      console.error('Failed to load audio:', error);
+    });
+};

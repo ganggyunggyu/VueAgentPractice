5e1c7c8eb27a550f63013d8be7da07eb425a245f
ganggyunggyu <qwzx16@naver.com>
2024-06-25 18:51:13 +0900

    feat: complate views add

---
+++ src/views/Complate.vue
@@ -0,0 +1,26 @@
+<script setup>
+  import { useImageDataStore } from '@/stores/imageData';
+  import TopNavigation from '@/features/busan-coex/TopNavigation.vue';
+  import BottomNavigation from '@/features/complate/BottomNavigation.vue';
+  const imageStore = useImageDataStore();
+  const imageData = imageStore.imageData;
+</script>
+<template>
+  <TopNavigation />
+  <div class="frame">
+    <img :src="`${imageData}`" alt="" />
+  </div>
+  <BottomNavigation />
+</template>
+<style scoped>
+  .frame {
+    position: absolute;
+    top: calc(10 * var(--vh));
+    width: 100%;
+    height: calc(75 * var(--vh));
+  }
+  .frame img {
+    width: 100%;
+    height: 100%;
+  }
+</style>

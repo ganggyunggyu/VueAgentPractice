cd9d3fbd68f067171357dbaf5786c8fa6ee2b352
ganggyunggyu <qwzx16@naver.com>
2024-06-27 11:10:39 +0900

    css: eventItem 약 수정

--- src/entities/stage/ui/EventDetailModal.vue
+++ src/entities/stage/ui/EventDetailModal.vue
@@ -1,27 +1,27 @@
 <script setup>
-import { getUrl } from '@/shared/lib/getUrl';
-import { setVh } from '@/shared/lib/setVh';
-import { onMounted } from 'vue';
+  import { getUrl } from '@/shared/lib/getUrl';
+  import { setVh } from '@/shared/lib/setVh';
+  import { onMounted } from 'vue';
 
-const props = defineProps({
-  event: {
-    type: Object,
-    required: true,
-  },
-  isModal: {
-    type: Boolean,
-    required: true,
-  },
-});
-const emit = defineEmits(['update:isModal']);
+  const props = defineProps({
+    event: {
+      type: Object,
+      required: true,
+    },
+    isModal: {
+      type: Boolean,
+      required: true,
+    },
+  });
+  const emit = defineEmits(['update:isModal']);
 
-const closeModal = () => {
-  emit('update:isModal', false);
-};
+  const closeModal = () => {
+    emit('update:isModal', false);
+  };
 
-onMounted(() => {
-  setVh();
-});
+  onMounted(() => {
+    setVh();
+  });
 </script>
 
 <template>
@@ -43,7 +43,11 @@ onMounted(() => {
       </div>
     </article>
     <article class="bottom-container">
-      <img class="logo" src="../../../../static/resource/common/event_logo.png" alt="logo-image" />
+      <img
+        class="logo"
+        src="../../../../static/resource/common/event_logo.png"
+        alt="logo-image"
+      />
       <img
         width="250px"
         height="250px"
@@ -53,84 +57,88 @@ onMounted(() => {
         alt="reeward-image"
       />
       <p>{{ props.event.content4 }}</p>
-      <p>{{ props.event.content5 }}</p>
+      <p class="small-text">{{ props.event.content5 }}</p>
     </article>
   </section>
 </template>
 
 <style scoped>
-section {
-  width: 90%;
-  height: calc(75 * var(--vh));
-  background-color: white;
-  z-index: 999;
-  position: fixed;
-  top: calc(14 * var(--vh));
-  border-radius: 16px;
-  border: 2px solid var(--color-red);
-  padding: 20px;
-  display: flex;
-}
-.top-container {
-  display: flex;
-  gap: 40px;
-  padding: 20px 0;
-}
-.bottom-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background-color: #eee;
-  padding: 20px;
-  border-radius: 16px;
-}
+  section {
+    width: 90%;
+    height: calc(75 * var(--vh));
+    background-color: white;
+    z-index: 999;
+    position: fixed;
+    top: calc(14 * var(--vh));
+    border-radius: 16px;
+    border: 2px solid var(--color-red);
+    padding: 20px;
+    display: flex;
+  }
+  .top-container {
+    display: flex;
+    gap: 40px;
+    padding: 20px 0;
+  }
+  .bottom-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: #eee;
+    padding: 20px;
+    border-radius: 16px;
+  }
 
-.icon {
-  padding: 10px;
-  width: 70px;
-  height: 70px;
-  border-radius: 50%;
-  background-color: var(--color-landing-pink);
-}
-.logo {
-  width: 80%;
-}
-.reward {
-  width: 80%;
-}
-.title-container {
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-  gap: 3px;
-}
-.content1 {
-  color: var(--color-red);
-  font-size: 20px;
-  font-weight: 800;
-}
-.content2 {
-  color: grey;
-}
-.content3 {
-  font-size: 18px;
-}
-.close-button {
-  position: absolute;
-  top: 10px;
-  right: 20px;
-  padding: 5px;
-  transition: all 0.3s;
-}
-.close-button:hover {
-  scale: 1.2;
-  background: #eee;
-  border-radius: 16px;
-}
-.close-button:active {
-  scale: 1.2;
-  background: #eee;
-  border-radius: 16px;
-}
+  .icon {
+    padding: 10px;
+    width: 70px;
+    height: 70px;
+    border-radius: 50%;
+    background-color: var(--color-landing-pink);
+  }
+  .logo {
+    width: 80%;
+  }
+  .reward {
+    width: 80%;
+  }
+  .title-container {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    gap: 3px;
+  }
+  .content1 {
+    color: var(--color-red);
+    font-size: 20px;
+    font-weight: 800;
+  }
+  .content2 {
+    color: grey;
+  }
+  .content3 {
+    font-size: 18px;
+  }
+  .close-button {
+    position: absolute;
+    top: 10px;
+    right: 20px;
+    padding: 5px;
+    transition: all 0.3s;
+  }
+  .close-button:hover {
+    scale: 1.2;
+    background: #eee;
+    border-radius: 16px;
+  }
+  .close-button:active {
+    scale: 1.2;
+    background: #eee;
+    border-radius: 16px;
+  }
+  .small-text {
+    font-size: 12px;
+    padding-top: 10px;
+  }
 </style>
